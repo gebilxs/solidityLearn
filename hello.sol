950fec16c2db0fd69c5743c3fe6da573461b6fec
徐踔锟 <1799676669@qq.com>
2023-08-02 03:30:11 +0800

    add hello.sol

---
+++ hello.sol
@@ -0,0 +1,17 @@
+
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.4;
+  /**
+   * @title Helloworld
+   * @dev ContractDescription
+   * @custom:dev-run-script scripts/deploy_with_ethers.ts
+   */
+contract Helloworld{
+    string Myname = "xck";
+
+    function getName()  public view returns (string memory){
+        return Myname;
+    }
+    
+}
