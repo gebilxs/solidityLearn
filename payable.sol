d390996d4949539bbf1f17368fffe507b53a0100
徐踔锟 <1799676669@qq.com>
2023-08-02 06:55:28 +0800

    update payable.sol

--- payable.sol
+++ payable.sol
@@ -10,4 +10,21 @@ pragma solidity ^0.8.4;
 
 contract payableTest {
     function pay()public payable {}
+    function getBalance () public view returns (uint){
+        return address(this).balance;
+    }
+    function getThis() public view returns(address){
+        return address(this);
+    }
+    function getRBalance(address account) public view returns (uint){
+        return account.balance;
+    }
+    function transfer()public payable  {
+        address payable account = payable(0x5B38Da6a701c568545dCfcB03FcB875f56beddC4);
+        account.transfer(address(this).balance);
+    }
+      function transfer2(address payable recipient, uint amount) public {
+        require(amount <= address(this).balance, "Insufficient balance");
+        recipient.transfer(amount);
+    }
 }
